edf3b4e138fc58eb4500e5a2a8a357d338f40b50
lizbeth-cortes <lizbethcortes317@aragon.unam.mx>
2024-03-17 20:11:50 -0600

    Más ejercicios de JS usando HTML, CSS

---
+++ listado_personas/js/app.js
@@ -0,0 +1,28 @@
+const personas = [
+    new Persona ('Juan','Perez'),
+    new Persona ('Karla','Lara'),
+];
+
+function mostrarPersonas(){
+    let texto = '';
+    for (let persona of personas) {
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`
+    }
+
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if (nombre.value !='' && apellido.value !='') {
+        const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+    } else{
+        console.log('No hay información que agregar');
+    }
+    
+}
